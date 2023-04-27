87dbd2758288e369adfc884166e4764efc987b5d
aSipz <115708388+aSipz@users.noreply.github.com>
2023-04-27 16:59:45 +0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ config = {
     production: {
         PORT: 3030,
         SECRET: 'evenMoreSecretSecret',
-        DB_URI: 'mongodb://127.0.0.1:27017/furniture'
+        DB_URI: 'mongodb+srv://angel:ARQGwKviE7ImnraZ@cluster0.lqrboul.mongodb.net/test'
     }
 };
 
